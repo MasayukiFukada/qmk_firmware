799576771935ba2193632bb327c0414951804bb3
MasayukiFukada <m.fukada0813@gmail.com>
2020-06-14 14:25:51 +0900

    Alpha の追加

---
+++ keyboards/alpha/keymaps/minamo/custom_keys.h
@@ -0,0 +1,49 @@
+
+#define MY_A    TD(TD_KEY_A)
+#define MY_B    KC_B
+#define MY_C    KC_C
+#define MY_D    KC_D
+#define MY_E    TD(TD_KEY_E)
+#define MY_F    TD(TD_KEY_F)
+#define MY_G    KC_G
+#define MY_H    KC_H
+#define MY_I    TD(TD_KEY_I)
+#define MY_J    KC_J
+#define MY_K    KC_K
+#define MY_L    TD(TD_KEY_L)
+#define MY_M    KC_M
+#define MY_N    TD(TD_KEY_N)
+#define MY_O    TD(TD_KEY_O)
+#define MY_P    TD(TD_KEY_P)
+#define MY_Q    TD(TD_KEY_Q)
+#define MY_R    KC_R
+#define MY_S    KC_S
+#define MY_T    KC_T
+#define MY_U    TD(TD_KEY_U)
+#define MY_V    KC_V
+#define MY_W    TD(TD_KEY_W)
+#define MY_X    TD(TD_KEY_X)
+#define MY_Y    TD(TD_KEY_Y)
+#define MY_Z    KC_Z
+
+#define MY_1    TD(TD_KEY_1)
+#define MY_2    TD(TD_KEY_2)
+#define MY_3    TD(TD_KEY_3)
+#define MY_4    TD(TD_KEY_4)
+#define MY_5    TD(TD_KEY_5)
+#define MY_6    TD(TD_KEY_6)
+#define MY_7    TD(TD_KEY_7)
+#define MY_8    TD(TD_KEY_8)
+#define MY_9    TD(TD_KEY_9)
+#define MY_0    TD(TD_KEY_0)
+
+#define MY_CMM  TD(TD_KEY_COMM)
+#define MY_DOT  TD(TD_KEY_DOT)
+#define MY_SLSH TD(TD_KEY_SLSH)
+#define MY_SCLN TD(TD_KEY_SCLN)
+#define MY_MINS TD(TD_KEY_MINS)
+#define MY_QUOT TD(TD_KEY_QUOT)
+
+#define MY_SPC  TD(TD_KEY_SPC)
+#define MY_ENT  TD(TD_KEY_ENT)
+
