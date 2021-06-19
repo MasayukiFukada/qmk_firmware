787d17d483319082c399585188fd4d588199266a
MasayukiFukada <m.fukada0813@gmail.com>
2021-06-19 21:41:21 +0900

    lain をちょっと微調整

--- keyboards/dm9records/lain/keymaps/minamo/keymap.c
+++ keyboards/dm9records/lain/keymaps/minamo/keymap.c
@@ -43,45 +43,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
         KC_ESC , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_LBRC,   KC_J   , KC_L   , KC_U   , KC_Y   , KC_MINS, KC_BSPC, \
         KC_TAB , KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_RBRC,   KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , KC_DEL,  \
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,            KC_INS , KC_K   , KC_H   , KC_COMM, KC_LDOT, KC_LSLS, \
+        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,            KC_PSCR, KC_K   , KC_H   , KC_COMM, KC_LDOT, KC_LSLS, \
         KC_LADJ,                   KC_LALT, KC_LGUI, KC_LLOW, KC_MSPC,   KC_MENT, KC_LRAI, KC_RALT, KC_LSUP, KC_LFUN, KC_LCUR  \
     ),
     [LOWER] = LAYOUT(
         XXXXXXX, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+        XXXXXXX,                   _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______, _______ \
     ),
     [RAISE] = LAYOUT(
         XXXXXXX, KC_GRV , KC_QUOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, KC_EQL , KC_MINS, KC_BSLS, KC_SCLN, XXXXXXX, \
         XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, KC_COMM, KC_DOT , KC_SLSH, XXXXXXX, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+        XXXXXXX,                   _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______, _______ \
     ),
     [CURSOR] = LAYOUT(
         XXXXXXX, KC_PGUP, KC_HOME, KC_UP  , KC_END , XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+        _______, XXXXXXX, XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+        XXXXXXX,                   _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______, _______ \
     ),
     // RESET に失敗するとネジを全て緩める必要があるので複数用意しておく。
     [FUNCTION] = LAYOUT(
         RESET  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,   KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+        XXXXXXX,                   _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______, _______ \
     ),
     [SUPPORT] = LAYOUT(
         RESET  , KC_ESC , KC_INS , KC_HENK, KC_MHEN, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, KC_TAB , KC_DEL , KC_BSPC, KC_SPC , XXXXXXX, XXXXXXX,   KC_ENT , KC_ENT , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+        XXXXXXX,                   _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______, _______ \
     ),
     [ADJUST] = LAYOUT(
         RESET  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-        _______,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+        _______,                   _______, _______, _______, _______,   _______, XXXXXXX, _______, _______, _______, _______ \
     )
 };
 
