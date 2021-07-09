5e7216453dc81638e443aca5888d36e5fc3b3974
MasayukiFukada <m.fukada0813@gmail.com>
2021-07-09 22:21:18 +0900

    Iris を更新
    * 両手とも rev2 で焼くこと(配線の都合上)

--- keyboards/keebio/iris/keymaps/minamo/keymap.c
+++ keyboards/keebio/iris/keymaps/minamo/keymap.c
@@ -4,16 +4,16 @@
 
 extern keymap_config_t keymap_config;
 
-#define _QWERTY 0
+#define _BASE   0
 #define _LOWER 1
 #define _RAISE 2
-#define _EXTEND 3
+#define _CURSOR 3
 #define _FUNC  4
 #define _MOUSE 5
 #define _ADJUST 6
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
+  BASE = SAFE_RANGE,
   LOWER,
   RAISE,
   MOUSE,
@@ -26,19 +26,16 @@ enum custom_keycodes {
 #define KC________ KC_TRNS
 #define KC_XXXXXXX KC_NO
 
-#define KC_M_SPACE CTL_T(KC_SPC)
-#define KC_M_ENT SFT_T(KC_ENT)
-#define KC_M_LOWR LT(_LOWER, KC_ESC)
-#define KC_M_RASE LT(_RAISE, KC_BSPC)
-#define KC_M_ALT ALT_T(KC_BSPC)
+#define KC_MSPC CTL_T(KC_SPC)
+#define KC_MENT SFT_T(KC_ENT)
+#define KC_LOWR LT(_LOWER, KC_ESC)
+#define KC_RASE LT(_RAISE, KC_BSPC)
+#define KC_LDOT LT(_CURSOR, KC_DOT)
+#define KC_MALT ALT_T(KC_BSPC)
 
-#define KC_DFNC LT(_FUNC, KC_D)
-#define KC_GMOU LT(_MOUSE, KC_G)
-#define KC_FEXT LT(_EXTEND, KC_F)
-
-#define KC_LOWR LOWER
-#define KC_RASE RAISE
 #define KC_ADJ  ADJUST
+#define KC_MOSE MOUSE
+#define KC_FUNC FUNC
 #define KC_RST RESET
 #define KC_BL_S BL_STEP
 #define KC_DBUG DEBUG
@@ -62,72 +59,72 @@ extern rgblight_config_t rgblight_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT_kc(
+  [_BASE] = LAYOUT(
   //.-------+-------+-------+-------+-------+-------.                .-------+-------+-------+-------+-------+-------.
-     ESC    ,LCTRL  ,LALT   ,DEL    ,PGUP   ,PGDN   ,                 LEFT   ,DOWN   ,UP     ,RGHT   ,HOME   ,END    ,
-     TAB    ,Q      ,W      ,E      ,R      ,T      ,                 Y      ,U      ,I      ,O      ,P      ,BSPC   ,
-     LCTL   ,A      ,S      ,DFNC   ,FEXT   ,GMOU   ,                 H      ,J      ,K      ,L      ,SCLN   ,QUOT   ,
-     LSFT   ,Z      ,X      ,C      ,V      ,B      ,ADJ    , ADJ    ,N      ,M      ,COMM   ,DOT    ,SLSH   ,ESC    ,
+     KC_ESC ,XXXXXXX,XXXXXXX,XXXXXXX,KC_INS ,KC_DEL ,                 KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,XXXXXXX,KC_BSPC,
+     KC_TAB ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_B   ,                 KC_J   ,KC_L   ,KC_U   ,KC_Y   ,KC_MINS,XXXXXXX,
+     KC_LSFT,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_G   ,                 KC_M   ,KC_N   ,KC_E   ,KC_I   ,KC_O   ,KC_MHEN,
+     KC_ADJ ,KC_Z   ,KC_X   ,KC_C   ,KC_D   ,KC_V   ,KC_MOSE, KC_MOSE,KC_K   ,KC_H   ,KC_COMM,KC_LDOT,KC_SLSH,KC_HENK,
   //`-------+-------+-------+-------+-------+-------+-------||-------+-------+-------+-------+-------+-------+-------'
-                                     LGUI   ,M_LOWR ,M_SPACE, M_ENT  ,M_RASE ,M_ALT
+                                     KC_LGUI,KC_LOWR,KC_MSPC, KC_MENT,KC_RASE,KC_MALT
   ),
 
-  [_LOWER] = LAYOUT_kc(
+  [_LOWER] = LAYOUT(
   //.-------+-------+-------+-------+-------+-------.                .-------+-------+-------+-------+-------+-------.
-     F1     ,F2     ,F3     ,F4     ,F5     ,F6     ,                 F7     ,F8     ,F9     ,F10    ,F11    ,F12    ,
-     _______,TILD   ,BSLS   ,_______,_______,TAB    ,                 QUOT   ,DQT    ,UNDS   ,PIPE   ,COLN   ,_______,
-     _______,GRV    ,EQL    ,PLUS   ,MINS   ,APP    ,                 LBRC   ,RBRC   ,LCBR   ,RCBR   ,SCLN   ,_______,
-     _______,_______,_______,_______,_______,_______,_______, _______,LPRN   ,RPRN   ,LABK   ,RABK   ,QUES   ,_______,
+     _______,_______,_______,_______,_______,_______,                 _______,_______,_______,_______,_______,_______,
+     KC_EQL ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,                 KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
+     _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                 KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,_______,
+     _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______, _______,_______,_______,KC_COMM,KC_DOT ,KC_SLSH,_______,
   //`-------+-------+-------+-------+-------+-------+-------||-------+-------+-------+-------+-------+-------+-------'
                                      _______,_______,_______, _______,_______,_______
   ),
 
-  [_RAISE] = LAYOUT_kc(
+  [_RAISE] = LAYOUT(
   //.-------+-------+-------+-------+-------+-------.                .-------+-------+-------+-------+-------+-------.
-     _______,_______,_______,_______,_______,_______,                 _______,_______,_______,_______,_______,_______,
-     EQL    ,EXLM   ,AT     ,HASH   ,DLR    ,PERC   ,                 CIRC   ,AMPR   ,ASTR   ,LPRN   ,RPRN   ,_______,
-     _______,1      ,2      ,3      ,4      ,5      ,                 6      ,7      ,8      ,9      ,0      ,_______,
-     _______,PLUS   ,MINS   ,ASTR   ,SLSH   ,DOT    ,_______, _______,_______,_______,COMM   ,DOT    ,SLSH   ,_______,
+     KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                 KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
+     _______,KC_GRV ,KC_QUOT,XXXXXXX,XXXXXXX,XXXXXXX,                 XXXXXXX,KC_EQL ,KC_MINS,KC_BSLS,KC_SCLN,_______,
+     _______,XXXXXXX,KC_LBRC,KC_RBRC,XXXXXXX,XXXXXXX,                 XXXXXXX,XXXXXXX,KC_COMM,KC_DOT ,KC_SLSH,_______,
+     _______,_______,_______,_______,_______,_______,_______, _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,
   //`-------+-------+-------+-------+-------+-------+-------||-------+-------+-------+-------+-------+-------+-------'
                                      _______,_______,_______, _______,_______,_______
   ),
 
-  [_EXTEND] = LAYOUT_kc(
+  [_CURSOR] = LAYOUT(
   //.-------+-------+-------+-------+-------+-------.                .-------+-------+-------+-------+-------+-------.
      _______,_______,_______,_______,_______,_______,                 _______,_______,_______,_______,_______,_______,
-     _______,_______,_______,ESC    ,_______,_______,                 COLN   ,SCLN   ,LABK   ,RABK   ,QUES   ,_______,
-     _______,_______,_______,BSPC   ,_______,_______,                 ENT    ,SPC    ,COMM   ,DOT    ,SLSH   ,_______,
+     _______,KC_PGUP,KC_HOME,KC_UP  ,KC_END ,_______,                 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,
+     _______,KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,_______,                 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,_______,
      _______,_______,_______,_______,_______,_______,_______, _______,_______,_______,_______,_______,_______,_______,
   //`-------+-------+-------+-------+-------+-------+-------||-------+-------+-------+-------+-------+-------+-------'
                                      _______,_______,_______, _______,_______,_______
   ),
 
-  [_FUNC] = LAYOUT_kc(
+  [_FUNC] = LAYOUT(
   //.-------+-------+-------+-------+-------+-------.                .-------+-------+-------+-------+-------+-------.
      _______,_______,_______,_______,_______,_______,                 _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                 _______,_______,_______,_______,_______,_______,
-     _______,_______,_______,_______,_______,_______,                 LEFT   ,DOWN   ,UP     ,RIGHT  ,_______,_______,
-     _______,_______,_______,_______,_______,_______,_______, _______,HOME   ,END    ,_______,_______,_______,_______,
+     _______,_______,_______,_______,_______,_______,                 KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,_______,
+     _______,_______,_______,_______,_______,_______,_______, _______,KC_HOME,KC_END ,_______,_______,_______,_______,
   //`-------+-------+-------+-------+-------+-------+-------||-------+-------+-------+-------+-------+-------+-------'
                                      _______,_______,_______, _______,_______,_______
   ),
 
-  [_MOUSE] = LAYOUT_kc(
+  [_MOUSE] = LAYOUT(
   //.-------+-------+-------+-------+-------+-------.                .-------+-------+-------+-------+-------+-------.
-     _______,_______,_______,_______,_______,_______,                 _______,_______,_______,_______,_______,_______,
-     _______,_______,_______,ESC    ,_______,_______,                 BTN1   ,BTN2   ,BTN3   ,BTN4   ,BTN5   ,_______,
-     _______,_______,_______,BSPC   ,_______,_______,                 MS_L   ,MS_D   ,MS_U   ,MS_R   ,_______,_______,
-     _______,_______,_______,_______,_______,_______,_______, _______,WH_L   ,WH_D   ,WH_U   ,WH_R   ,_______,_______,
+     _______,_______,_______,_______,_______,_______,                 KC_ACL0,KC_ACL1,KC_ACL2,_______,_______,_______,
+     _______,_______,_______,KC_ESC ,_______,_______,                 KC_BTN1,KC_BTN2,KC_BTN3,KC_BTN4,KC_BTN5,_______,
+     _______,_______,_______,KC_BSPC,_______,_______,                 KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,
+     _______,_______,_______,_______,_______,_______,_______, _______,KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,_______,_______,
   //`-------+-------+-------+-------+-------+-------+-------||-------+-------+-------+-------+-------+-------+-------'
                                      _______,_______,_______, _______,_______,_______
   ),
 
-  [_ADJUST] = LAYOUT_kc(
+  [_ADJUST] = LAYOUT(
   //.-------+-------+-------+-------+-------+-------.                .-------+-------+-------+-------+-------+-------.
-     RST    ,_______,_______,_______,_______,_______,                 ACL0   ,ACL1   ,ACL2   ,_______,_______,RST    ,
-     _______,_______,_______,_______,_______,_______,                 BTN1   ,BTN2   ,BTN3   ,BTN4   ,_______,_______,
-     _______,_______,_______,_______,_______,_______,                 MS_L   ,MS_D   ,MS_U   ,MS_R   ,_______,_______,
-     BL_S   ,_______,_______,_______,_______,_______,_______, _______,WH_L   ,WH_D   ,WH_U   ,WH_R   ,_______,_______,
+     RESET  ,_______,_______,_______,_______,_______,                 KC_ACL0,KC_ACL1,KC_ACL2,_______,_______,RESET  ,
+     _______,_______,_______,_______,_______,_______,                 KC_BTN1,KC_BTN2,KC_BTN3,KC_BTN4,KC_BTN5,_______,
+     _______,_______,_______,_______,_______,_______,                 KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,
+     _______,_______,_______,_______,_______,_______,_______, _______,KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,_______,_______,
   //`-------+-------+-------+-------+-------+-------+-------||-------+-------+-------+-------+-------+-------+-------'
                                      _______,_______,_______, _______,_______,_______
   )
@@ -143,9 +140,9 @@ void persistent_default_layer_set(uint16_t default_layer) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
+    case BASE:
       if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_QWERTY);
+        persistent_default_layer_set(1UL<<_BASE);
       }
       return false;
       break;
