366b80b3979e995b8362aa2787a8113295ff3d91
Masayuki Fukada <m.fukada0813@gmail.com>
2024-05-18 14:03:23 +0900

    :wrench: レツプリを最新環境に *

--- keyboards/lets_split/keymaps/minamo/config.h
+++ keyboards/lets_split/keymaps/minamo/config.h
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#include "../../config.h"
+#include "../../rev2/config.h"
 
 /* Use I2C or Serial, not both */
 
@@ -42,7 +42,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_SLEEP
-#define RGBLED_NUM 12
+//2024.05.18 不要そうなので削除
+//#define RGBLED_NUM 12
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
