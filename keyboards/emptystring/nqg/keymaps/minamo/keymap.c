d327e37801189a54ccd929bff288a097f1bb1f41
Masayuki Fukada <m.fukada0813@gmail.com>
2024-05-18 22:35:43 +0900

    NQG 調整中

--- keyboards/emptystring/nqg/keymaps/minamo/keymap.c
+++ keyboards/emptystring/nqg/keymaps/minamo/keymap.c
@@ -46,6 +46,9 @@ tap_dance_action_t tap_dance_actions[] = {};
 #define KC_LLOW LT(_LOWER, KC_ESC)
 #define KC_LRAI LT(_RAISE, KC_BSPC)
 
+#define MY_ENT LSFT_T(KC_ENT)
+#define MY_SPC LCTL_T(KC_SPC)
+
 #define KC_HENK KC_INT4
 #define KC_MHEN KC_INT5
 #define RESET QK_BOOT
@@ -57,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                MY_Q    , MY_W    , MY_F    , MY_P    , KC_B    , KC_J    , MY_L    , MY_U    , MY_Y    , MY_MINS , \
                KC_A    , MY_R    , KC_S    , MY_T    , KC_G    , KC_M    , KC_N    , MY_E    , MY_I    , MY_O    , \
                KC_Z    , KC_X    , KC_C    , KC_D    , KC_V    , KC_K    , KC_H    , KC_COMM , KC_ENT  , KC_SLSH , \
-     KC_ESC  ,                     XXXXXXX , XXXXXXX , KC_SPC  , KC_ENT  , XXXXXXX , XXXXXXX
+     KC_ESC  ,                     XXXXXXX , KC_TAB  , MY_SPC  , MY_ENT  , KC_BSPC , _______
 // ,---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------.
   ),
   [_LOWER] = LAYOUT(
@@ -65,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ , _______ , _______ , _______ , _______ , \
                KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_EQL  , KC_SCLN , KC_COMM , KC_DOT  , KC_SLSH , \
                KC_GRV  , KC_QUOT , KC_LBRC , KC_RBRC , KC_BSLS , KC_PSCR , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , \
-     XXXXXXX ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+     _______ ,                     _______ , _______ , _______ , _______ , _______ , _______
 // ,---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------.
   ),
   [_RAISE] = LAYOUT(
@@ -73,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_PGUP , KC_HOME , KC_UP   , KC_END  , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , \
                KC_PGDN , KC_LEFT , KC_DOWN , KC_RGHT , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , \
                XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F13  , KC_F14  , KC_F15  , KC_F16  , KC_F17  , KC_F18  , \
-     XXXXXXX ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+     _______ ,                     _______ , _______ , _______ , _______ , _______ , _______
 // ,---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------.
   ),
   [_PLAY] = LAYOUT(
@@ -81,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , \
                KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , XXXXXXX , \
                XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , \
-     XXXXXXX ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+     _______ ,                     _______ , _______ , _______ , _______ , _______ , _______
 // ,---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------.
   ),
   [_SUPPORT] = LAYOUT(
@@ -89,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_ESC  , KC_INS  , KC_INT4 , KC_INT5 , XXXXXXX , XXXXXXX , XXXXXXX , _BASE   , _PLAY   , QK_BOOT , \
                KC_TAB  , KC_DEL  , KC_BSPC , KC_SPC  , XXXXXXX , XXXXXXX , KC_ENT  , XXXXXXX , KC_LNG5 , XXXXXXX , \
                XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_MUTE , KC_VOLU , KC_VOLD , XXXXXXX , XXXXXXX , \
-     XXXXXXX ,                     XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+     _______ ,                     _______ , _______ , _______ , _______ , _______ , _______
 // ,---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------.
   ),
 };
